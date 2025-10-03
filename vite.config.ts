e6f1d35093939084ba35143fdd63772593b15e7f
4rtamis <artamis@hackademint.org>
2025-10-03 15:07:34 +0200

    chore: add _redirects

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "https://bibqr.ravenloft.fr/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
